26c057357bd761c66b47c8ea69d2ffd7743668ff
userquin <userquin@gmail.com>
2024-08-24 11:10:02 +0200

    chore: test new Vite 5.4.2 sass modern stuff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,57 @@
 import vuetify from 'vite-plugin-vuetify';
 
 export default defineNuxtConfig({
+  compatibilityDate: '2024-08-23',
   css: ['vuetify/styles', '@/assets/main.scss'],
   build: {
     transpile: ['vuetify']
   },
   vite: {
+    plugins: [{
+      name: 'check',
+      enforce: 'post',
+      configResolved(config) {
+        console.log(config.css)
+      }
+    }],
+    css: {
+      devSourcemap: true,
+      preprocessorOptions: {
+        sass: {
+          api: 'modern-compiler'
+        },
+        /* prepare some tests for vuetify styles plugin: virtual
+        scss: {
+          api: 'modern',
+          importers: [
+            {
+              canonicalize(url: string) {
+                console.log('URL: ' + url)
+                return url === 'virtual-dep'
+                    ? new URL('custom-importer:virtual-dep')
+                    : null
+              },
+              load() {
+                console.log('WTF')
+                return {
+                  contents: ``,
+                  syntax: 'scss',
+                }
+              },
+            },
+          ]
+        }
+        */
+      },
+      // preprocessorMaxWorkers: true,
+    },
     ssr: {
       noExternal: ['vuetify']
     }
   },
   devtools: { enabled: true },
   features: {
+    inlineStyles: false,
     devLogs: false,
   },
   modules: [
